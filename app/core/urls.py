be493bedbfeb0c23b5477334578ee761970eb15d
Alexandre Silva <alexandresilva933@gmail.com>
2021-04-05 09:00:53 -0300

    #feat Criação das páginas e rotas de login e sign up

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'core'
 
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.Index.as_view(), name='index'),
+    # path('make_login/', views.make_login, name='make_login'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
+    path('sign-up/', views.SignUp.as_view(), name='sign_up'),
+    path('add_user/', views.add_user, name='add_user'),
+    path('current_weather/<str:city>/', views.current_weather, name='current_weather')
 ]
--- app/core/views.py
+++ app/core/views.py
@@ -1,5 +1,52 @@
-from django.shortcuts import render
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
+from django.views.generic import TemplateView, RedirectView
+from .open_weather_api.services import get_current_weather
 
 
-def index(requests):
-    return render(requests, 'core/index.html')
+class Index(LoginRequiredMixin, TemplateView):
+    login_url = 'core:login'
+    template_name = 'core/index.html'
+
+
+class Login(auth_views.LoginView):
+    template_name = 'core/login.html'
+
+
+class Logout(auth_views.LogoutView):
+    next_page = 'core:index'
+
+
+class SignUp(TemplateView):
+    template_name = 'core/sign_up.html'
+
+# def index(request):
+#     return render(request, 'core/index.html')
+
+
+@csrf_protect
+def add_user(request):
+    return HttpResponse(request, 'add user test')
+
+
+@login_required(login_url='core:login')
+def current_weather(request, city: str):
+    if request.method == 'GET':
+        weather = dict(get_current_weather(city))
+        print(weather)
+        print(type(weather))
+        return JsonResponse(weather)
+
+
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('core:index')
+    else:
+        return render(request, 'core/login.html')
+
+
