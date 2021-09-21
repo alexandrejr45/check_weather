124af4c83aab4c1aa33dcb8382a31c23b351f6db
Alexandre Silva <alexandresilva933@gmail.com>
2021-09-21 20:01:43 -0300

    feat: Add User model and UserAdmin on django admin panel

--- app/core/urls.py
+++ app/core/urls.py
@@ -7,10 +7,8 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
-    # path('make_login/', views.make_login, name='make_login'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
     path('sign-up/', views.SignUp.as_view(), name='sign_up'),
-    path('add_user/', views.add_user, name='add_user'),
-    path('current_weather/<str:city>/', views.current_weather, name='current_weather')
+    path('current_weather/<str:city>/', views.CheckWeather.as_view(), name='current_weather')
 ]
